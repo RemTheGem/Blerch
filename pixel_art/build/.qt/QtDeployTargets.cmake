90dc80d07251777cdd8b4cd638eb364139ae0b78
Rem-69 <aa902af41e@gmail.com>
2026-08-05 14:42:42 +1000

    removed old folder

--- pixel_art/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_pixel_art_FILE /home/rem/programming/pixel_art/pixel_art/build/pixel_art)
-set(__QT_DEPLOY_TARGET_pixel_art_TYPE EXECUTABLE)
--- pixel_art/build/CMakeFiles/pixel_art_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for pixel_art_autogen.
-# This may be replaced when dependencies are built.
