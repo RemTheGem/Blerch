b92552930dab71a3c49bff3a3ad7f24b0aec5c21
Rem-69 <aa902af41e@gmail.com>
2026-09-08 21:03:42 +1000

    FIX: method cleanup. got rid of redundant stuff

--- src/PixelCanvas.h
+++ src/PixelCanvas.h
@@ -49,10 +49,7 @@ public:
     void resetCanvas(); // reset the whole canvas
 
     void updateCanvasSize(); // update the current canvas size
-    void buildPalette(); // get colors and their corresponding frequencies from the canvas and add them to a hash table
     void floodFill(int x, int y); // method for flood fill tool
-    void undo(); // undo method
-    void redo(); // redo method
 
     void copyPixels(); // copy pixels
     void pastePixels(); // paste copied pixels
@@ -93,20 +90,8 @@ public:
         int height;
     };
 
-    // layer methods
-    void addLayer(); // add a new layer
-    void removeLayer(int index); // remove selected layer
-    void setActiveLayer(int index); // change selected layer
-    void moveLayerUp(int index); // move selected layer up
-    void moveLayerDown(int index); // move selected layer down
-    void renameLayer(int index, const QString &name); // rename selected layer
-    void setLayerOpacity(int index, float opacity); // change selected layer's opacity
-    float getLayerOpacity(int index)const; // get selected layer's opacity
-    QStringList getLayerNames(); // return the list of layer names
-
     // helper methods
     void paintColor(int x, int y, const QColor &color, bool recordUndo = true); // paint color into the corresponding square
-    QList<QColor> sortColors(QHash<QRgb, int> colorFrequency); // sort a list of colors based on frequency
     QColor getColor(); // return the current color
     int getZoom(); // get the current zoom
     void setZoom(int zoom); // set the zoom amount
@@ -118,9 +103,6 @@ public:
     void drawCircle(QPoint topLeft, QPoint bottomRight, bool recordUndo = true); // function to draw a circle
     void drawEllipse(QPoint topLeft, QPoint bottomRight, bool recordUndo = true); // function to draw an ellipse
     void drawLine(QPoint start, QPoint end, bool recordUndo = true); // function to draw a straight line
-    void makeTempLayer(); // make temporary layer for preview
-    void removeTempLayer(); // remove temporary layer
-    int getActiveLayer() const;
     void setBrushSize(int newSize); // change brush size
     void commitMove(); // confirm move
     void cancelMove(); // cancel move
@@ -146,17 +128,7 @@ public:
     QColor blendPixel(const QColor &color, const QColor &blendColor);
     void setBrushApplication(BrushApplication type);
     // frame methods
-    void duplicateFrame();
-    void addFrame();
-    void deleteFrame(int index);
-    void copyFrame();
-    void pasteFrame();
     void switchFrame(int index);
-    int getCurrentFrame();
-    int getFrameSize();
-    int getFrameDuration();
-    void setFrameDuration(int value);
-    QImage renderFrame(int frameIndex);
 
     // autosave
     bool autosaveDirty = false;
@@ -179,7 +151,6 @@ private:
     QPoint moveOffset; // point on the canvas for moving picture
     QColor currentColor = Qt::black; // selected color
     bool isDrawing = false; // bool to check if user is drawing
-    bool isUndoing = false; // bool to check if user is undoing
     bool isErasing = false; // bool to check if user is erasing
     bool isPasting = false;
     bool darkMode = true;
@@ -197,13 +168,11 @@ private:
     // others
 
 
-    QHash<QRgb, int> colorFrequency; // number of times color has appeared on current canvas
     Tool currentTool = Tool::Brush; // default tool
     ShapeType currentShape = ShapeType::Rectangle; // default shape
     Selection selection;
     Shape shape;
     QVector<PixelChange> currentAction; // vector to store current action
-    int maxUndo = 5; // max number of undos (probably redundant atp. cbf to check)
 signals:
     void colorChanged(QColor color); // signal to change the selected color
     void mousePositionChanged(int x, int y); // signal to change current mouse position in status bar
